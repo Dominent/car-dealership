1b338e6f1a7268463b1d25fc1b9af4446b389e53
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2021-06-02 19:26:30 +0200

    Update css and add order vehicle button

--- src/pages/vehicles.jsx
+++ src/pages/vehicles.jsx
@@ -4,27 +4,40 @@ import { GET_VEHICLES } from "../queries";
 
 const Vehicles = () => {
   const { loading, data } = useQuery(GET_VEHICLES);
+  const orderVehicle = (vehicle) => {
+    console.log(vehicle);
+  };
 
   if (loading) {
     return <h1>Loading Vehicles...</h1>;
   } else {
     console.log(data);
     return (
-      <div className="vehicles">
+      <div id="vehicles">
         {data.vehicles.map((vehicle) => {
           return (
-            <div className="vehicle" key={vehicle.id}>
-              <p>{`Name: ${vehicle.name}`}</p>
-              <img
-                className="vehicle-image"
-                src={vehicle.image.url}
-                alt={`Tesla ${vehicle.image.filename}`}
-              ></img>
-              <p>{`Price: ${vehicle.price}`}</p>
-              <p>{`Mileage: ${vehicle.mileage}`}</p>
-              <p>{`Vin: ${vehicle.vin}`}</p>
-              <p>{`Year: ${vehicle.year}`}</p>
-              <p className="vehicle-description">{`${vehicle.description}`}</p>
+            <div id="vehicle" key={vehicle.id}>
+              <h4 id="vehicle-name">{vehicle.name}</h4>
+              <div id="vehicle-image-stats">
+                <img
+                  id="vehicle-image"
+                  src={vehicle.image.url}
+                  alt={`Tesla ${vehicle.image.filename}`}
+                ></img>
+                <div id="vehicle-stats">
+                  <p id="vehicle-price">{`Price: ${vehicle.price}`}</p>
+                  <p id="vehicle-mileage">{`Mileage: ${vehicle.mileage}`}</p>
+                  <p id="vehicle-vin">{`Vin: ${vehicle.vin}`}</p>
+                  <p id="vehicle-year">{`Year: ${vehicle.year}`}</p>
+                </div>
+              </div>
+              <p id="vehicle-description">{`${vehicle.description}`}</p>
+              <button
+                id="vehicle-orderbutton"
+                onClick={() => orderVehicle(vehicle)}
+              >
+                Order vehicle
+              </button>
             </div>
           );
         })}
